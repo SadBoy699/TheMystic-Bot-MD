38f141767be5c54d9a3632ab6401b46d232633d8
SadBoy699 <105267410+SadBoy699@users.noreply.github.com>
2022-06-11 17:16:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['17722386341']
+  ['994402650973', '👑 Mystic - Creador 👑', true],
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
